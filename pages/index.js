99f3c6b318593dd3ccb8474aacf4bd676354f8a0
MarinJursic <marin.jursic@gmail.com>
2021-12-23 23:12:13 +0100

    priority

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
 import styles from "../styles/Home.module.scss";
 import background from "../public/background/free-mountain-vector-24.webp";
