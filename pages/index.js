3563782eb171f467ac2a8b8044586e6d4eefcb8b
MarinJursic <marin.jursic@gmail.com>
2021-12-23 23:40:48 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import background from "../public/background/free-mountain-vector-24.webp";
 import github from "../public/images/Github.svg";
 import linkedin from "../public/images/linkedin.svg";
