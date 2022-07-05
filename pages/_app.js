6a5cb5d064786d912320b2bf49076ed83ef8bbca
MarinJursic <marin.jursic@gmail.com>
2022-07-05 14:34:23 +0200

    Added hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,19 @@ function MyApp({ Component, pageProps }) {
        `}
       </Script>
 
+      <Script strategy="lazyOnload">
+        {` <!-- Hotjar Tracking Code for https://www.marinjursic.me/ -->
+            (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3052465,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+         `}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
